d30dc87b52632bd284ebd3b8c427c3ab0dce65b3
zhangchao04 <zhangchao04@huohua.cn>
2022-11-16 20:17:23 +0800

    fix: 优化代码格式

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,5 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect, createRef } from 'react';
+import { useEffect, useState, createRef } from 'react';
 // import icon from '../../assets/icon.svg';
 import './App.css';
 
@@ -12,23 +12,23 @@ import { message, Upload } from 'antd';
 const { Dragger } = Upload;
 
 const props: UploadProps = {
-  name: 'file',
-  multiple: true,
-  action: '/Users/zhangchao/Downloads',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
+    name: 'file',
+    multiple: true,
+    action: '/Users/zhangchao/Downloads',
+    onChange(info) {
+        const { status } = info.file;
+        if (status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
+    },
+    onDrop(e) {
+        console.log('Dropped files', e.dataTransfer.files);
+    },
 };
 
 // const App: React.FC = () => (
@@ -95,80 +95,82 @@ const props: UploadProps = {
 // };
 
 export default function App() {
+    // 创建组件引用
+    let dropRef = createRef();
 
-  // 创建组件引用
-  let dropRef = createRef();
+    const { isFile, setFile } = useState(true);
 
 
-  // const background = document.getElementsByClassName('background');
-  // background.addEventListener("dragover", e => {
-  //   // 必须阻止默认事件，这里才会变成可释放区域。
-  //   // 如果不写这一句，那么松手的时候，也不会触发相应的 drop 事件
-  //   e.preventDefault();
-  // });
+    // const background = document.getElementsByClassName('background');
+    // background.addEventListener("dragover", e => {
+    //   // 必须阻止默认事件，这里才会变成可释放区域。
+    //   // 如果不写这一句，那么松手的时候，也不会触发相应的 drop 事件
+    //   e.preventDefault();
+    // });
 
-  useEffect(() => {
-    // 监听拖放事件
-    const dropArea = dropRef.current;
-    dropArea.addEventListener("dragover",  e => {
-      e.preventDefault();
+    useEffect(() => {
+        // 监听拖放事件
+        const dropArea: any = dropRef.current;
+        dropArea.addEventListener('dragover',  (e: any) => {
+            e.preventDefault();
+        });
 
-      console.log('122222');
-    });
+        dropArea.addEventListener('drop', (e: any) => {
+            let items = e.dataTransfer.items;
+            for (let i = 0; i <= items.length - 1; i++) {
+                let item = items[i];
+                if (item.kind === 'file') {
+                    // FileSystemFileEntry 或 FileSystemDirectoryEntry 对象
+                    let entry = item.webkitGetAsEntry();
+                    // 递归地获取entry下包含的所有File
+                    getFileFromEntryRecursively(entry);
+                }
+            }
+            e.preventDefault();
+        });
+    }, []);
 
-    dropArea.addEventListener("drop", e => {
-      let items = e.dataTransfer.items;
-      for (let i = 0; i <= items.length - 1; i++) {
-        let item = items[i];
-        if (item.kind === "file") {
-          // FileSystemFileEntry 或 FileSystemDirectoryEntry 对象
-          let entry = item.webkitGetAsEntry();
-          // 递归地获取entry下包含的所有File
-          getFileFromEntryRecursively(entry);
-        }
-      }
-      e.preventDefault();
-    });
-  });
-
-  return (
-    <div ref={dropRef}>
-      <div className="background">
-        拖入DYSM符号文件
-      </div>
-      {/* <Dragger {...props}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">TEST</p>
-        <p className="ant-upload-hint">TEST</p>
-      </Dragger> */}
-      <h1>electron-react-boilerplate</h1>
-    </div>
-  );
+    return (
+        <div ref = { dropRef }>
+        <div className="background">
+            拖入DYSM符号文件
+        </div>
+        {/* <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">TEST</p>
+            <p className="ant-upload-hint">TEST</p>
+        </Dragger> */}
+        <h1>electron-react-boilerplate</h1>
+        </div>
+    );
 }
 
 
-function getFileFromEntryRecursively(entry) {
-  console.log(entry);
-  if (entry.isFile) {
-    entry.file(
-      file => {
-        // addFileToList({ file, path: entry.fullPath });
-        console.log(file.path);
-        console.log(`${file.path}${entry.fullPath}`);
-      },
-      e => { console.log(e); }
-    );
-  } else {
-    let reader = entry.createReader();
-    reader.readEntries(
-      entries => {
-        entries.forEach(entry => getFileFromEntryRecursively(entry));
-      },
-      e => { console.log(e); }
-    );
-  }
+function getFileFromEntryRecursively(entry: any) {
+    console.log(entry);
+    if (entry.isFile) {
+        entry.file(
+            file => {
+                // addFileToList({ file, path: entry.fullPath });
+                console.log(file.path);
+                console.log(`${file.path}${entry.fullPath}`);
+            },
+            e => { console.log(e); }
+        );
+    } else {
+
+        // let reader = entry.createReader();
+        // reader.readEntries(
+        //     entries => {
+        //         entries.forEach(entry => getFileFromEntryRecursively(entry));
+        //     },
+        //     e => {
+        //         console.log(e);
+        //     }
+        // );
+    }
 }
 
 // {/* // <Router>
