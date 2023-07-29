e0d8ef132da350178e05a5a211c0d066fcef4eb6
zhangchao <zhangchao@vv.cn>
2023-07-29 14:08:42 +0800

    fix: 更新eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  extends: 'erb',
+    extends: 'erb',
     rules: {
         // A temporary hack related to IDE not resolving correct package.json
-        'import/no-extraneous-dependencies': 'off',
-        'import/no-unresolved': 'error',
+        // 'import/no-extraneous-dependencies': 'off',
+        // 'import/no-unresolved': 'error',
         // Since React 17 and typescript 4.1 you can safely disable the rule
-        'react/react-in-jsx-scope': 'off',
-        'generator-star-spacing': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'allowForLoopAfterthoughts': true
+        // 'react/react-in-jsx-scope': 'off',
+        // 'generator-star-spacing': 'off',
+        // '@typescript-eslint/no-explicit-any': 'off',
+        // 'allowForLoopAfterthoughts': true
     },
     parserOptions: {
         ecmaVersion: 2020,
@@ -22,7 +22,9 @@ module.exports = {
             // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
             node: {},
             webpack: {
-                config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
+                config: require.resolve(
+                    './.erb/configs/webpack.config.eslint.ts'
+                ),
             },
             typescript: {},
         },
